cba8ce2a9e936ed63873d4953744706455f693c1
pedrinho-matador <112881797+pedrinho-matador@users.noreply.github.com>
2024-04-16 10:24:35 -0300

    jnjn

--- main.js
+++ main.js
@@ -12,5 +12,8 @@ for(let i = 0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "contagem regressiva";
+const tempoObjetivo1 = new Date("2024-12-01");
+let tempoAtual =new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
